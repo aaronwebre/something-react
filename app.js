5551e8261da6689b79bb4e90a826b08c322d809e
Aaron Webre <aaron.webre@g5platform.com>
2019-07-14 10:48:30 -0700

    Props

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
-      <h1>Scoreboard</h1>
-      <span className="stats">Players: 1</span>
+      <h1>{ props.title }</h1>
+      <span className="stats">Players: { props.totalPlayers }</span>
     </header>
   );
 }
 
-const Player = () => {
+const Player = (props) => {
   return (
     <div className="player">
-      <span className="player-name">
-        Aaron
-      </span>
-      <Counter />
+      <span className="player-name">{ props.name }</span>
+      <Counter score={ props.score } />
     </div>
   );
 }
 
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">00</span>
+      <span className="counter-score">{ props.score }</span>
       <button className="counter-action increment"> + </button>
     </div>
   );
@@ -31,10 +29,16 @@ const Counter = () => {
 const App = () => {
   return (
     <div className="scoreboard">
-      <Header />
+      <Header 
+        title="Scoreboard"
+        totalPlayers={1} 
+      />
 
       {/* Players list */}
-      <Player />
+      <Player 
+        name="Aaron"
+        score={13}
+      />
     </div>
   );
 }
