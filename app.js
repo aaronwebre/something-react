2bc89a8eaf8924d654b3e11d94cc27c84ded5e62
Aaron Webre <aaron.webre@g5platform.com>
2019-07-15 07:43:31 -0700

    Event handlers

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 const players = [
   {
     name: "Aaron",
-    score: 50,
     id: 1
   },
   {
     name: "Faron",
-    score: 85,
     id: 2
   },
   {
     name: "Zaron",
-    score: 95,
     id: 3
   },
   {
     name: "Haron",
-    score: 80,
     id: 4
   }
 ]
@@ -43,12 +39,25 @@ class Counter extends React.Component {
   state = {
     score: 0
   }
+
+  incrementScore = () => {
+    this.setState({
+      score: this.state.score + 1
+    });
+  }
+
+  decrementScore = () => {
+    this.setState({
+      score: this.state.score - 1
+    });
+  }
+
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement"> - </button>
+        <button className="counter-action decrement" onClick={ this.decrementScore }> - </button>
         <span className="counter-score">{ this.state.score }</span>
-        <button className="counter-action increment"> + </button>
+        <button className="counter-action increment" onClick={ this.incrementScore }> + </button>
       </div>
     );
   } 
