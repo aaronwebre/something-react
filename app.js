644aff24a261863e6b65d4d957e3a66257d829e6
Aaron Webre <aaron.webre@g5platform.com>
2019-07-14 22:11:51 -0700

    State class property

--- app.js
+++ app.js
@@ -40,11 +40,8 @@ const Player = (props) => {
 }
 
 class Counter extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      score: 0
-    };
+  state = {
+    score: 0
   }
   render() {
     return (
